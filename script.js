27a49df61c47ba5f2e771525a2f49911f7631f47
KEVIN BABU <bighero.0.0.7.0@gmail.com>
2023-05-01 18:26:50 +0530

    height

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ const hero = document.querySelector('[data-hero]')
 
 window.addEventListener('mousemove', (e) => {
 	const { clientX, clientY } = e
-  const x = Math.round((clientX / window.innerWidth) * 100)
-  const y = Math.round((clientY / window.innerHeight) * 100)
+  const x = ((clientX / window.innerWidth) * 100)
+  const y =((clientY / window.innerHeight) * 100)
 	
 	hero.style.setProperty('--x', `${x}%`)
 	hero.style.setProperty('--y', `${y}%`)
